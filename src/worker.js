af4d40db583387dc91fb6ff253e792515c8d643b
Louis <louis@jurczyk.fr>
2019-01-18 15:52:10 +0200

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -16,19 +16,6 @@ queue.process('tweet', function(job, done) {
 	setTimeout(exec, 61637, job.data, done);
 });
 
-/*
-					job.type = this.wordIsPresent(words.retweet, text) ? job.type | this.retweet : job.type
-					job.type = this.wordIsPresent(words.follow, text) ? job.type | this.follow : job.type
-					job.type = this.wordIsPresent(words.tag, text) ? job.type | this.tag : job.type
-*/
-			//onsole.log(mask & this.retweet, mask & this.follow, mask & this.tag)
-
-			/*
-			if ( mask & this.FLAG_B) {
-				console.log("ok")
-			}
-			*/
-
 function exec(data, done) {
 	if (data.type & constants.follow) {
 		twitterAPI.follow(data.mention)
